9aa4addfe756c911ec80b46b032d108a5a15f70b
codingnoob0 <codingnoob1@gmail.com>
2020-05-19 02:10:12 +0530

    Updates

---
+++ precache-manifest.a7f45e93559158207f7cb41e64c0c35a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b09eb4a02763599bb994505f93dd267",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abbf2385b9b2675b29f0",
+    "url": "/static/css/main.68487a33.chunk.css"
+  },
+  {
+    "revision": "665f0b24cde1919c7d54",
+    "url": "/static/js/2.63783066.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.63783066.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abbf2385b9b2675b29f0",
+    "url": "/static/js/main.97389b26.chunk.js"
+  },
+  {
+    "revision": "d3be38e7f455fb7c073c",
+    "url": "/static/js/runtime-main.25917a94.js"
+  }
+]);
